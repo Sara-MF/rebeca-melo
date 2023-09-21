23e4227c8843655b3c8779ee8a25a6974dbf88c6
Sara-MF <sara.melo@aluno.uece.br>
2023-09-21 10:10:30 -0300

    Making a page to each project

--- app/page.jsx
+++ app/page.jsx
@@ -10,12 +10,12 @@ export default function Home() {
 
             <div className="projects">
                 <Project name="Projeto 1" link={"/projeto1"} />
-                <Project name="Projeto 2" link={"/projeto1"} />
-                <Project name="Projeto 3" link={"/projeto1"} />
-                <Project name="Projeto 4" link={"/projeto1"} />
-                <Project name="Projeto 5" link={"/projeto1"} />
-                <Project name="Projeto 6" link={"/projeto1"} />
-                <Project name="Projeto 7" link={"/projeto1"} />
+                <Project name="Projeto 2" link={"/projeto2"} />
+                <Project name="Projeto 3" link={"/projeto3"} />
+                <Project name="Projeto 4" link={"/projeto4"} />
+                <Project name="Projeto 5" link={"/projeto5"} />
+                <Project name="Projeto 6" link={"/projeto6"} />
+                <Project name="Projeto 7" link={"/projeto7"} />
             </div>
         </main>
     )
---
+++ app/projeto4/page.jsx
@@ -0,0 +1,49 @@
+import Image from "next/image"
+
+export default function Projeto4() {
+    return (
+        <main className="content">
+
+            <div className="py-20 px-40">
+
+                <div className="relative">
+
+                    <Image
+                        src="/landscape.png"
+                        alt="My picture"
+                        width={2000}
+                        height={400}
+                        className="mb-8"
+                    />
+
+                    <p className="text-grape font-extralight text-2xl flex flex-start mb-12">Tipo do projeto</p>
+
+                    <div className="flex flex-row text-justify gap-16 mb-32">
+                        <h4 className="text-grape font-normal text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet nulla facilisi morbi tempus iaculis urna id volutpat. Sed arcu non odio euismod. Purus non enim praesent elementum. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Consectetur adipiscing elit ut aliquam purus sit amet luctus venenatis. Sit amet luctus venenatis lectus magna. Praesent semper feugiat nibh sed. Scelerisque eu ultrices vitae auctor eu augue. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Dignissim cras tincidunt lobortis feugiat vivamus.</h4>
+
+                        <h4 className="text-grape font-normal text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet nulla facilisi morbi tempus iaculis urna id volutpat. Sed arcu non odio euismod. Purus non enim praesent elementum. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Consectetur adipiscing elit ut aliquam purus sit amet luctus venenatis. Sit amet luctus venenatis lectus magna. Praesent semper feugiat nibh sed. Scelerisque eu ultrices vitae auctor eu augue. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Dignissim cras tincidunt lobortis feugiat vivamus.</h4>
+                    </div>
+
+                    <Image
+                        src="/landscape.png"
+                        alt="My picture"
+                        width={2000}
+                        height={400}
+                        className="mb-8"
+                    />
+
+                    <p className="text-grape font-extralight text-2xl flex flex-start mb-12">Tipo do projeto</p>
+
+                    <div className="flex flex-row text-justify gap-16">
+                        <h4 className="text-grape font-normal text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet nulla facilisi morbi tempus iaculis urna id volutpat. Sed arcu non odio euismod. Purus non enim praesent elementum. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Consectetur adipiscing elit ut aliquam purus sit amet luctus venenatis. Sit amet luctus venenatis lectus magna. Praesent semper feugiat nibh sed. Scelerisque eu ultrices vitae auctor eu augue. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Dignissim cras tincidunt lobortis feugiat vivamus.</h4>
+
+                        <h4 className="text-grape font-normal text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet nulla facilisi morbi tempus iaculis urna id volutpat. Sed arcu non odio euismod. Purus non enim praesent elementum. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Consectetur adipiscing elit ut aliquam purus sit amet luctus venenatis. Sit amet luctus venenatis lectus magna. Praesent semper feugiat nibh sed. Scelerisque eu ultrices vitae auctor eu augue. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Dignissim cras tincidunt lobortis feugiat vivamus.</h4>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </main>
+    )
+}
