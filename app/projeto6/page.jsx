313f73db0b5fd98947dcbad9101db11e745fd5b2
Sara-MF <sara.melo@aluno.uece.br>
2023-09-22 08:16:06 -0300

    Adding style to link that go back to projects page. Adjusting space between content in each project page

--- app/projeto6/page.jsx
+++ app/projeto6/page.jsx
@@ -7,7 +7,9 @@ export default function Projeto6() {
 
             <div className="py-10 px-40">
 
-                <Link href={"/"} className="text-grape font-extralight text-lg flex flex-start pb-7">Voltar</Link>
+                <div className="bg-sand rounded-3xl w-20 h-7 items-center mb-7">
+                    <Link href={"/"} className="text-grape font-extralight text-lg">Voltar</Link>
+                </div>
 
                 <div className="relative">
 
@@ -19,7 +21,7 @@ export default function Projeto6() {
                         className="mb-8"
                     />
 
-                    <p className="text-grape font-extralight text-2xl flex flex-start mb-12">Tipo do projeto</p>
+                    <p className="text-grape font-extralight text-2xl flex flex-start mb-20">Tipo do projeto</p>
 
                     <div className="flex flex-row text-justify gap-16 mb-32">
                         <h4 className="text-grape font-normal text-xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet nulla facilisi morbi tempus iaculis urna id volutpat. Sed arcu non odio euismod. Purus non enim praesent elementum. Velit sed ullamcorper morbi tincidunt ornare massa eget egestas purus. Consectetur adipiscing elit ut aliquam purus sit amet luctus venenatis. Sit amet luctus venenatis lectus magna. Praesent semper feugiat nibh sed. Scelerisque eu ultrices vitae auctor eu augue. Tempor commodo ullamcorper a lacus vestibulum sed arcu non. Dignissim cras tincidunt lobortis feugiat vivamus.</h4>
@@ -45,7 +47,9 @@ export default function Projeto6() {
 
                 </div>
 
-                <Link href={"/"} className="text-grape font-extralight text-lg flex flex-start pt-12">Voltar</Link>
+                <div className="bg-sand rounded-3xl w-20 h-7 items-center mt-12">
+                    <Link href={"/"} className="text-grape font-extralight text-lg">Voltar</Link>
+                </div>
 
             </div>
 
